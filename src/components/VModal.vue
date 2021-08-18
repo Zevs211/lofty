672a91f5d607a482a7402944d5f9de944b6e4885
Fox758 <Zevs211>
2021-08-18 16:33:03 +0300

    first commit

---
+++ src/components/VModal.vue
@@ -0,0 +1,86 @@
+<template>
+  <div class="v-modal" @click.self="closeModal">
+    <form class="modal-form">
+      <input v-model="postBody.username" />
+      <input v-model="postBody.password" />
+      <button @click="login">SEND</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+export default {
+  name: "VModal",
+  data() {
+    return {
+      postBody: {
+        username: "",
+        password: "",
+      },
+    };
+  },
+  methods: {
+    closeModal() {
+      this.$emit("on-modal-close");
+    },
+
+    async login() {
+      const response = await axios.post(
+        "https://reqres.in/api/articles",
+        this.postBody
+      );
+      console.log(response);
+    },
+    // postPost() {
+    //   const str = JSON.stringify(this.postBody);
+    //   axios.post("https://jsonplaceholder.typicode.com/users", str).then((response) => {
+    //     console.log(response);
+    //   });
+    // },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.v-modal {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  background-color: rgba(0, 0, 0, 0.3);
+}
+.modal-form {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  width: 240px;
+  height: 208px;
+  padding: 15px;
+  background-color: #222831;
+  & > input {
+    outline: none;
+    height: 30px;
+    margin-top: 20px;
+    color: #fff;
+    background-color: #51596b;
+  }
+  button {
+    width: 100px;
+    height: 60px;
+    margin-top: 20px;
+    background-color: #dfa612;
+    &:hover {
+      user-select: none;
+      cursor: pointer;
+      background-color: #ecc86d;
+    }
+  }
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="wrapper">
+    <div class="header">
+      <div class="header__content">
+        <button class="btn" @click="openModal">Sign up</button>
+      </div>
+    </div>
+    <div class="container">
+      <div class="container__item"></div>
+      <div class="container__item"></div>
+      <div class="container__item"></div>
+      <div class="container__item"></div>
+      <div class="container__item"></div>
+      <div class="container__item"></div>
+    </div>
+    <transition name="fade">
+      <v-modal v-if="isModalVisible" @on-modal-close="closeModal" />
+    </transition>
+  </div>
+</template>
+
+<script>
+import VModal from "@/components/VModal.vue";
+
+export default {
+  name: "Home",
+  components: { VModal },
+  data() {
+    return {
+      isModalVisible: false,
+    };
+  },
+  methods: {
+    openModal() {
+      this.isModalVisible = true;
+    },
+    closeModal() {
+      this.isModalVisible = false;
+    },
+  },
+};
+</script>
